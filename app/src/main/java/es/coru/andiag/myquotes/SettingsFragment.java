3f71d4384fc42607d3ea033d5d91e8afd614abb7
Iago <Santos Domnguez>
2016-02-05 23:46:29 +0100

    Arreglado bug simple  + limpieza codigo

--- app/src/main/java/es/coru/andiag/myquotes/SettingsFragment.java
+++ app/src/main/java/es/coru/andiag/myquotes/SettingsFragment.java
@@ -42,6 +42,7 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
                     public void onClick(DialogInterface dialog, int which) {
                         isDialogOpen = false;
                         dialog.dismiss();
+                        getActivity().recreate();
                     }
                 });
         alertDialog.show();
@@ -54,22 +55,18 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
                 getActivity().recreate();
                 break;
             case Global.PREF_MUST_SYNC:
-                if (Global.isLite()) { //If u are a lite user u can´t disable the synchronization
-                    if (!isDialogOpen) { //It avoid the app to create more than one dialog
-                        isDialogOpen = true;
-                        showDialog("INSERTAR STRING AQUI");
-                    }
+                if (Global.isLite() && !isDialogOpen) { //If u are a lite user u can´t disable the synchronization
+                    isDialogOpen = true;
+                    showDialog("INSERTAR STRING AQUI");
                     //Put the value again to true
                     SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
                     prefs.edit().putBoolean(Global.PREF_MUST_SYNC, true).apply();
                 }
                 break;
             case Global.PREF_SYNC_LANGUAGES:
-                if (Global.isLite()) { //If u are a lite user u can´t disable the synchronization
-                    if (!isDialogOpen) { //It avoid the app to create more than one dialog
-                        isDialogOpen = true;
-                        showDialog("INSERTAR OTRO STRING AQUI");
-                    }
+                if (Global.isLite() && !isDialogOpen) { //If u are a lite user u can´t disable the synchronization
+                    isDialogOpen = true;
+                    showDialog("INSERTAR OTRO STRING AQUI");
                     //Put the value again to all languages
                     SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
                     prefs.edit().putStringSet(Global.PREF_SYNC_LANGUAGES, Global.defaultSyncLanguages).apply();
