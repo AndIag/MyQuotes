65e47900aa5e2eda7cecc5d3eb2b3d31060f3cc2
Iago <Santos Domnguez>
2016-02-15 19:46:37 +0100

    changes in types

--- app/src/main/java/es/coru/andiag/myquotes/entities/QuoteType.java
+++ app/src/main/java/es/coru/andiag/myquotes/entities/QuoteType.java
@@ -6,6 +6,6 @@ import java.io.Serializable;
  * Created by iagoc on 06/02/2016.
  */
 public enum QuoteType implements Serializable {
-    MOVIE, MUSIC, PERSONAL, BOOK, DEFAULT
+    DEFAULT, MUSIC, PERSONAL, BOOK, MOVIE
             /*0,1,2,3,4*/
 }
